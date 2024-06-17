3ba711b3220a14fb0f6b51edbac925a366510838
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2024-06-18 05:57:00 +0700

    Make inference.js load Python model

--- src/app.js
+++ src/app.js
@@ -1,13 +1,57 @@
 const Hapi = require('@hapi/hapi');
-const { loadModel } = require('./inference');
+const { recommend } = require('./inference');
 
 (async () => {
-    const model = await loadModel();
-    console.log('Model loaded!');
-    
     const server = Hapi.server({
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-        port: 8000
+        port: 8000,
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+        },
+    });
+
+    server.ext('onPreResponse', (request, h) => {
+        const response = request.response;
+        if (response.isBoom && response.output.statusCode === 413) {
+            return h.response({
+                "status": "fail",
+                "message": "Payload content length greater than maximum allowed: 1000000"
+             }).code(413);
+        }
+        return h.continue;
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/recommend',
+        handler: async (request, h) => {
+            try {
+                const { skills } = request.payload;
+
+                console.log(skills)
+
+                const recommendations = await recommend(skills);
+
+                console.log(recommendations)
+            } catch (err) {
+                console.log(err);
+
+                return h.response({
+                    "status": "fail",
+                    "message": "Terjadi kesalahan dalam melakukan prediksi"
+                }).code(400);
+            }
+        },
+        
+        options: {
+            payload: {
+                allow: 'multipart/form-data',
+                multipart: true,
+                maxBytes: 1000000
+            }
+        }
     });
 
     await server.start();
--- src/inference.js
+++ src/inference.js
@@ -1,8 +1,22 @@
-const tfjs = require('@tensorflow/tfjs-node');
+const { PythonShell } = require('python-shell');
+const path = require('path');
 
-function loadModel() {
-    const modelUrl = "file://models/model.json";
-    return tfjs.loadLayersModel(modelUrl);
+const pythonPath = 'python3';
+const scriptPath = 'models/first.py';
+
+async function recommend(skills) {
+    options = {
+        pythonPath: pythonPath,
+        args: [skills]
+    };
+
+    try {
+        const results = await PythonShell.run(scriptPath, options);
+        return results;
+    } catch (err) {
+        console.error('Error in Python script:', err);
+        throw err;
+    }
 }
 
-module.exports = { loadModel };
+module.exports = { recommend };
